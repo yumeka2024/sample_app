a356b1deb17e7225d537903cbafaa5c289074017
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-02-08 05:34:39 +0000

    [Finish]3-6

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   #List.find(1) #listsテーブルの中にあるidが1のレコードを取得
   #/lists/1    => List.find(params[:id]) => idが1のレコードを取得
   get 'lists/:id' => 'lists#show', as: 'list'
+  
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
